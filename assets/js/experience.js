ff75418bc1b417bc3c2d38a7efc79714b5013755
kaleraja <69078920+kaleraja@users.noreply.github.com>
2025-04-08 20:12:52 +0545

    Add files via upload

---
+++ assets/js/experience.js
@@ -0,0 +1,58 @@
+AOS.init();
+
+//  Work experience cards
+
+
+const showCards2 = () => {
+  let output = "";
+  exp.forEach(
+    ({ title, cardImage, place, time, desp }) =>
+      (output += `        
+    <div class="col gaap" data-aos="fade-up" data-aos-easing="linear" data-aos-delay="100" data-aos-duration="400"> 
+      <div class="card card1">
+        <img src="${cardImage}" class="featured-image"/>
+        <article class="card-body">
+          <header>
+            <div class="title">
+              <h3>${title}</h3>
+            </div>
+            <p class="meta">
+              <span class="pre-heading">${place}</span><br>
+              <span class="author">${time}</span>
+            </p>
+            <ol>
+              ${desp}
+            </ol>
+          </header>
+        </article>
+      </div>
+    </div>
+      `)
+  );
+  experiencecards.innerHTML = output;
+};
+document.addEventListener("DOMContentLoaded", showCards2);
+
+
+
+const showCards3 = () => {
+  let output = "";
+  mentor.forEach(
+    ({ title, image, subtitle, desp, href }) =>
+      (output += `  
+      <div class="blog-slider__item swiper-slide">
+        <div class="blog-slider__img">
+            <img src="${image}" alt="">
+        </div>
+        <div class="blog-slider__content">
+          <div class="blog-slider__title">${title}</div>
+          <span class="blog-slider__code">${subtitle}</span>
+          <div class="blog-slider__text">${desp}</div>
+          <a href="${href}" class="blog-slider__button">Read More</a>   
+        </div>
+      </div>
+      `)
+  );
+  hackathonsection.innerHTML = output;
+};
+document.addEventListener("DOMContentLoaded", showCards3);
